aae457426e0a695b014e952fb24a5502849163be
asd412id <asdar.binsyam@gmail.com>
2020-09-27 23:58:29 +0800

    Tweak layout on download detail nilai

--- app/Modules/Admin/Views/nilai/detail-download.blade.php
+++ app/Modules/Admin/Views/nilai/detail-download.blade.php
@@ -78,7 +78,7 @@
                   </tr>
                 </table>
               </td>
-              <td valign="top" width="300">
+              <td valign="top" width="400">
                 <table class="table table-info">
                   <tr>
                     <td>MATA PELAJARAN</td>
